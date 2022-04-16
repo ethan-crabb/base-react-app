ea871b57d141bc9bd03100353814f1d644b925c3
Crabby12439 <ethancrabb@outlook.com>
2022-04-16 11:57:24 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Basic create-react-app setup
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './Styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './Pages/Home/Home';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
